b5a30751f2fd1afef4bdf4d2b59e50da45227b01
brennan <brennan.mcna@gmail.com>
2025-02-03 22:34:43 -0500

    Small enemy tweaks. Hitbox & chase player behavior

--- Scenes & Scripts/Hero Gameplay Window/Enemies/chase_player.gd
+++ Scenes & Scripts/Hero Gameplay Window/Enemies/chase_player.gd
@@ -2,7 +2,8 @@ extends StateNode
 
 @onready var me : Enemy = $"../.."
 var _hero_location : Vector2
-var _vision_box : Rect2
+
+@export var noise : Noise #maybe add randomness later
 
 # Called by a StateMachine once it is ready.
 func _state_machine_ready() -> void:
@@ -16,14 +17,16 @@ func _enter_state(old_state, state_data: Dictionary):
 	pass
 
 var last_x # track last x coord to flip sprite accordingly
+var offset : Vector2 = Vector2(randf_range(10,30), randf_range(-10, 10))
 func _process(delta: float) -> void:
 	_slow_process(delta) #process, but runs slower to save performance
 	last_x = me.position.x
 	
-	var move_target = _hero_location + Vector2.RIGHT * 25 if me.position.x > _hero_location.x else _hero_location + Vector2.LEFT * 25
+	var move_target = _hero_location + offset if me.position.x > _hero_location.x else _hero_location - offset
+	
 	
 	me.position = me.position.move_toward(move_target, delta * me.move_speed)
-	
+
 	if me.position.x > last_x:
 		me.sprite.flip_h = true
 		me.hitbox.scale.x = -1
@@ -35,16 +38,14 @@ func _process(delta: float) -> void:
 
 
 #----
-var _process_every : float = .2 #run process every X seconds
+var _process_every : float = .5 #run process every X seconds
 var _process_timer_elapsed : float = randf_range(0, _process_every)
 func _slow_process(delta):
 	_process_timer_elapsed += delta
 	if _process_every <= _process_timer_elapsed :
 		_process_timer_elapsed = 0 #reset timer
 		_hero_location = get_player_location()
-		for collision in me.attack_range.get_overlapping_areas() :
-			if collision.is_in_group("hero"):
-				me._state_machine.enter_state("attack")
+
 
 func get_player_location() -> Vector2 :
 	return me._hero.position
@@ -52,3 +53,8 @@ func get_player_location() -> Vector2 :
 # Called by a StateMachine when the state is exited.
 func _exit_state(new_state, state_data: Dictionary):
 	pass
+
+
+func _on_attack_range_area_entered(area: Area2D) -> void:
+	if area.is_in_group("hero"):
+		me._state_machine.enter_state("attack")
